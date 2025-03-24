bf608c15260ab53cf6ef99454786329ca3d95b7d
dante <heywheredante@gmail.com>
2025-03-25 00:25:35 +0900

    feat: 20250325 추가

---
+++ pcce_1번ㅊ_출력.py
@@ -0,0 +1,29 @@
+#PCCE 기출문제 1번 / 출력
+# 제출 내역
+# 문제 설명
+# 주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+# 출력 예시
+# Spring is beginning
+# 13
+# 310
+
+string_msg = "Spring is beginning"
+
+int_val = 3
+
+string_val = "3"
+
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
+
+# 너가원하는 답은 주지 않겠다
+# string_msg = "Spring is beginning"
+# int_val = len(string_msg.split())
+# string_val = str(int_val)
+
+# print(string_msg)
+# print(int_val + 10)
+# print(string_val + "10")
